029dfd4af55d1f85b36aac6ecfce5b68ece89e53
Aleksey Lovchikov <svdmusic@gmail.com>
2015-04-22 14:58:50 +0400

    fix keys api insta

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ set_time_limit(0);
 ini_set('default_docket_timeout', 300);
 session_start();
 
-define('CLIENT_ID', '4724aca56eba4a08bd561a249664e42f');
-define('CLIENT_SECRET', '5e6ff4ab7e16449d89e5659556f4d3f5');
-define('REDIRECT_URI', 'http://localhost:63342/phpbook/index.php');
+define('CLIENT_ID', 'e56d1ca6c927498eba1d19f74cf46ded');
+define('CLIENT_SECRET', '8c690088725e40c1a4620e4c36a94f5d');
+define('REDIRECT_URI', 'https://uppics.herokuapp.com/');
 define('IMAGE_DIR', 'pics/');
 
 function if_login($code) {
