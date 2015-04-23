28c0cd50314065882609196d56923eecdffe52b5
Aleksey Lovchikov <svdmusic@gmail.com>
2015-04-23 16:34:24 +0400

    fix all

--- index.php
+++ index.php
@@ -138,6 +138,8 @@ if (preg_match($reg, $str)) {
 <html>
     <head>
         <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>UpPics</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
